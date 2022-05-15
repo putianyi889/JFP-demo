4c1301ead5c414ef413131bc77bf835dc1ea9807
Tianyi Pu <912396513@qq.com>
2022-05-15 18:06:08 +0100

    Update fractional_heat_wave_equation.jl

--- fractional_heat_wave_equation.jl
+++ fractional_heat_wave_equation.jl
@@ -6,7 +6,7 @@ pgfplotsx()
 # x dim
 x=Inclusion(0..2π);
 F=Fourier()[x,:];
-xgrid=0:0.005π:2π;
+xgrid=range(0,2π,length=201);
 K=40;
 xksol=F[xgrid,1:2*K+1]';
 
@@ -33,7 +33,7 @@ if abs(tv[end])>1e-15
     # check if N is large enough
     @warn "Possible loss of accuracy: the last coefficient is $(tv[end])."
 end
-tgridshift=0:0.01:2;
+tgridshift=range(0,2,length=201);
 tksol=zeros(length(tgridshift),2*K+1);
 fill!(view(tksol,:,1),1);
 for k=1:K
@@ -49,28 +49,104 @@ end
 sol=tksol*diagm(fv)*xksol;
 
 # interactive surface plot
-PlotlyJS.plot(
+plt=PlotlyJS.plot(
     PlotlyJS.surface(
         z=sol,y=xgrid,x=T/2*tgridshift,
         hidesurface=true,
+        showscale=false,
         contours=attr(
-            x_show=true, x_color="blue", x_size=1, x_start=0, x_end=T,
-            y_show=true, y_color="red", y_size=0.5π, y_start=0, y_end=2π
+            x_show=true, x_color="blue", x_size=0.5, x_start=0, x_end=T,
+            y_show=true, y_color="red", y_size=0.25π, y_start=0, y_end=2π
         )
     ),
-    Layout(size=(700,700), font_family="Times New Roman",
+    Layout(size=(700,700), font_family="Helvetica",
+        margin=attr(b=0,l=0,r=0,t=0),
         scene=attr(
-            aspectmode="cube", zaxis_backgroundcolor="black", 
-            xaxis_title="t", yaxis_title="x", zaxis_title="u",
-            yaxis_tickmode="array",
-            yaxis_tickvals=π*(0:0.5:2),
-            yaxis_ticktext=["0" "π/2" "π" "3π/2" "2π"]
+            aspectmode="cube",
+            camera_eye=attr(autoexpand=false,x=1.5,y=-1.5,z=1.5),
+            xaxis=attr(
+                title="<i>t</i>",
+                range=(0,8)
+            ),
+            yaxis=attr(
+                title="<i>x</i>",
+                tickmode="array",
+                tickvals=π*(0:0.5:2),
+                ticktext=["0" "π/2" "π" "3π/2" "2π"]
+            ),
+            zaxis=attr(
+                title="<i>u</i>",
+            )
         )
     )
 )
 
-# contour plot
-Plots.contour(xgrid, T/2*tgridshift, sol, xlabel="\$x\$", ylabel="\$t\$", xticks=((0:0.5π:2π),["0" "\$\\pi/2\$" "\$\\pi\$" "\$3\\pi/2\$" "\$2\\pi\$"]), xlims=(0,2π), framestyle=:box, size=(400,400))
+# contour plot (Plots.pgfplotsx and PlotlyJS)
+Plots.contour(Float16.(xgrid), Float16.(T/2*tgridshift), Float16.(sol), xlabel="\$x\$", ylabel="\$t\$", xticks=((0:0.5π:2π),["0" "\$\\pi/2\$" "\$\\pi\$" "\$3\\pi/2\$" "\$2\\pi\$"]), xlims=(0,2π), framestyle=:box, size=(300,270), colorbar=false)
+
+layout=Layout(
+    xaxis=attr(
+        title=attr(
+            text="<i>x</i>",
+            font_size=15,
+            font_family="Helvetica",
+            font_color="black",
+            standoff=5
+        ),
+        tickfont_family="Helvetica",
+        tickfont_size=10,
+        tickfont_color="black",
+        tickvals=π*(0:0.5:2),
+        ticktext=["0" "<i>π</i>/2" "<i>π</i>" "3<i>π</i>/2" "2<i>π</i>"],
+        mirror=true, showline=true,
+        linecolor="black",
+    ), 
+    yaxis=attr(
+        title=attr(
+            text="<i>t</i>",
+            font_size=15,
+            font_family="Helvetica",
+            font_color="black",
+            standoff=10
+        ),
+        tickfont_family="Helvetica",
+        tickfont_size=10,
+        tickfont_color="black",
+        #autorange="reversed",
+        mirror=true,
+        showline=true,
+        linecolor="black"
+    ), 
+    margin=attr(l=0,r=8,b=0,t=0),
+    plot_bgcolor="white",
+    width=300,
+    height=300
+)
+plt1=PlotlyJS.plot(
+    PlotlyJS.contour(
+        z=sol, x=xgrid, y=T/2*tgridshift, 
+        contours_showlabels=true, 
+        contours_coloring="lines", 
+        contours_labelfont_family="Helvetica",
+        contours_labelfont_size=8,
+        contours_start=-1, contours_size=0.2, contours_end=4,
+        connectgaps=true, 
+        line_smoothing=0.5, 
+        showscale=false, 
+        # colorscale=[[0, "black"], [1, "black"], ]
+    ),
+    layout
+)
 
 # wireframe plot
-Plots.wireframe(xgrid[1:10:end], T/2*tgridshift[1:10:end], sol[1:10:end,1:10:end], xlabel="\$x\$", ylabel="\$t\$", zlabel="u", xticks=((0:0.5π:2π),["0" "\$\\pi/2\$" "\$\\pi\$" "\$3\\pi/2\$" "\$2\\pi\$"]), xlims=(0,2π), framestyle=:box, size=(400,400), legend=false)
+l=(minimum(sol),maximum(sol))
+Plots.plot(size=(300,300), camera=(30,30), legend=false, xlims=(0,2π), ylims=(0,8), zlims=l, xlabel="\$x\$", ylabel="\$t\$", zlabel="\$u\$", xticks=((0:0.5π:2π),["0" "\$\\pi/2\$" "\$\\pi\$" "\$3\\pi/2\$" "\$2\\pi\$"]), foreground_color_grid=:gray90, gridalpha=1, framestyle=:box)
+for x in 1:8:length(xgrid)
+    Plots.plot!(Float16.(xgrid[x]*ones(length(tgridshift))),Float16.(T/2*tgridshift),Float16.(sol[:,x]), color=:blue, linewidth=0.3)
+end
+for t in 1:5:length(tgridshift)
+    Plots.plot!(Float16.(xgrid), Float16.(T/2*tgridshift[t]*ones(length(xgrid))), Float16.(sol[t,:]),color=:blue, linewidth=0.3)
+end
+Plots.plot!([0;0], [0;0], [l[1];l[2]], color=:black)
+Plots.plot!([0;0], [8;8], [l[1];l[2]], color=:black)
+Plots.plot!([2π;2π], [8;8], [l[1];l[2]], color=:black)
