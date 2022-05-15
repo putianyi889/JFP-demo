c19ff457d46db2bbb4272f2b1f373d780a339543
Tianyi Pu <912396513@qq.com>
2022-05-15 18:06:47 +0100

    Update foward_recurrence.jl

--- foward_recurrence.jl
+++ foward_recurrence.jl
@@ -14,24 +14,23 @@ layout=Layout(
         title=attr(
             text="column index",
             font_size=20,
-            font_family="Times New Roman",
+            font_family="Helvetica",
             font_color="black",
         ),
-        tickfont_family="Times New Roman",
+        tickfont_family="Helvetica",
         tickfont_size=15,
         tickfont_color="black",
         mirror=true, showline=true,
         linecolor="black",
-        side="top"
     ), 
     yaxis=attr(
         title=attr(
             text="row index",
             font_size=20,
-            font_family="Times New Roman",
+            font_family="Helvetica",
             font_color="black"
         ),
-        tickfont_family="Times New Roman",
+        tickfont_family="Helvetica",
         tickfont_size=15,
         tickfont_color="black",
         autorange="reversed",
@@ -39,6 +38,7 @@ layout=Layout(
         showline=true,
         linecolor="black"
     ), 
+    margin=attr(l=0,r=0,b=0,t=0),
     plot_bgcolor="white",
     width=400,
     height=400
@@ -50,7 +50,7 @@ plt1=plot(
         z=err21, 
         contours_showlabels=true, 
         contours_coloring="lines", 
-        contours_labelfont_family="Times New Roman",
+        contours_labelfont_family="Helvetica",
         contours_start=-80, contours_size=10, contours_end=80,
         connectgaps=true, 
         line_smoothing=0.5, 
@@ -66,7 +66,7 @@ plt2=plot(
         z=err22, 
         contours_showlabels=true, 
         contours_coloring="lines", 
-        contours_labelfont_family="Times New Roman",
+        contours_labelfont_family="Helvetica",
         contours_start=-80, contours_size=10, contours_end=80,
         connectgaps=true, 
         line_smoothing=0.5, 
