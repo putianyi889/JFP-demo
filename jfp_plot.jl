79627da527ce6c09dd77c683878ab496b94ea341
Tianyi Pu <912396513@qq.com>
2022-05-15 18:06:40 +0100

    Update jfp_plot.jl

--- jfp_plot.jl
+++ jfp_plot.jl
@@ -6,10 +6,10 @@ f=[x->S[x,n] for n in 2:5]
 g=[x->S[testmodule.x2y(x,2.0),n] for n in 2:5]
 
 # connection between x and y
-Plots.plot(x->sqrt(2*(1+x))-1, xlims=(-1,1), ylims=(-1,1), size=(250,250), legend=false, framestyle=:box, xlabel="\$x\$", ylabel="\$y\$")
+plot(x->sqrt(2*(1+x))-1, xlims=(-1,1), ylims=(-1,1), size=(250,250), legend=false, framestyle=:box, xlabel="\$x\$", ylabel="\$y\$")
 
 # Legendre polynomials
-Plots.plot(f, xlims=(-1,1), legend=false, framestyle=:box, size=(250,250), xlabel=L"$y$", linestyle=[:solid :dash :solid :dash], ylabel=L"$P_n(y)$")
+plot(f, xlims=(-1,1), legend=false, framestyle=:box, size=(250,250), xlabel=L"$y$", linestyle=[:solid :dash :solid :dash], ylabel=L"$P_n(y)$", ylims=(-1,1))
 
 # Legendre fractional polynomials
-Plots.plot(g, xlims=(-1,1), ylims=(-1,1), legend=false, framestyle=:box, size=(250,250), xlabel="\$x\$", ylabel=L"$Q_n^{(0,0,0,2)}(x)$", linestyle=[:solid :dash :solid :dash])
+plot(g, xlims=(-1,1), ylims=(-1,1), legend=false, framestyle=:box, size=(250,250), xlabel="\$x\$", ylabel=L"$Q_n^{(0,0,0,2)}(x)$", linestyle=[:solid :dash :solid :dash])
